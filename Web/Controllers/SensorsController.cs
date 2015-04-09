bc0818a7a47b413c5c75db86b3a8abd54fc552f8
Jeremiah O'Connor <straylightthegreat@gmail.com>
2015-04-09 09:35:52 -0500

    .net HTTP references and WebAPI controller for sensors

---
+++ Web/Controllers/SensorsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    public class SensorsController : ApiController
+    {
+        // GET api/sensors
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/sensors/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/sensors
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/sensors/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/sensors/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
